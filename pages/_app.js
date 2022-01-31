7b3483c7152a9c0edbeb1ecb515f742982062105
kevin bhavsar <kevinbhavsar567@gmail.com>
2022-01-31 15:16:45 +0530

    add redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,35 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import App from "next/app";
+import { Provider } from "react-redux";
+import withRedux from "next-redux-wrapper";
+import React from "react";
+import store from "../redux/store";
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+/*export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}*/
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    const appProps = Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {};
+
+    console.log(appProps);
+    return { appProps: appProps };
+  }
+
+  render() {
+    const { Component, appProps } = this.props;
+    return (
+      <Provider store={store}>
+        <Component {...appProps} />
+      </Provider>
+    );
+  }
 }
+
+const makestore = () => store;
+
+export default withRedux(makestore)(MyApp);
--- pages/login.js
+++ pages/login.js
@@ -1,45 +1,81 @@
-import Image from 'next/image'
-import styles from '../styles/login.module.css'
-import Link from 'next/link'
+import Image from "next/image";
+import styles from "../styles/login.module.css";
+import { connect } from "react-redux";
+import { setInfo } from "../redux/actions/main";
+import Link from "next/link";
 
-export default function Login(){
-	return(
-<html>		
-	<header>
-		<nav className={styles.nav}>
-			<Link href="/" passHref>
-				<div style={{cursor: 'pointer'}}>
-				<Image src={require('../public/logo.png')} alt="sparshlogo"/>
-				</div>
-			</Link>
-		</nav>
-	</header>
+function Login() {
+  return (
+    <html>
+      <header>
+        <nav className={styles.nav}>
+          <Link href="/" passHref>
+            <div style={{ cursor: "pointer" }}>
+              <Image src={require("../public/logo.png")} alt="sparshlogo" />
+            </div>
+          </Link>
+        </nav>
+      </header>
 
-	<body>
-		<div style={{height: 55+ 'px'}}></div>
-		<form action="action_page.php">
-			<div className={styles.container}>
-				<h1>Login</h1>
-				<p>Enter email (or) username and passsword.</p>
-				<hr className='hr'/>
-		
-				<label><b>Email/Username</b></label>
-				<br/>
-				<input type="text" placeholder="Enter Email/Username" name="email" id="email" className={styles.input} required/>
-				<br/>
+      <body>
+        <div style={{ height: 55 + "px" }}></div>
+        <form action="action_page.php">
+          <div className={styles.container}>
+            <h1>Login</h1>
+            <p>Enter email (or) username and passsword.</p>
+            <hr className="hr" />
 
-				<label><b>Password</b></label>
-				<br/>
-				<input type="password" placeholder="Enter Password" name="psw" id="psw" className={styles.input} required/>
-				<br/>
-				<button type="submit" className={styles.registerbtn}>Login</button>
-			</div>
-		
-			<div className={styles.instructions}>
-				<p>Contact <Link href="tel:+919409656394">+91 9409 6563 94</Link> or <Link href="mailto:lharsh175@gmail.com">lharsh175@gmail.com</Link> if you face any difficulties. </p>
-			</div>
-		</form>
-	</body>
-</html>
-)
+            <label>
+              <b>Email/Username</b>
+            </label>
+            <br />
+            <input
+              type="text"
+              placeholder="Enter Email/Username"
+              name="email"
+              id="email"
+              className={styles.input}
+              required
+            />
+            <br />
+
+            <label>
+              <b>Password</b>
+            </label>
+            <br />
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="psw"
+              id="psw"
+              className={styles.input}
+              required
+            />
+            <br />
+            <button type="submit" className={styles.registerbtn}>
+              Login
+            </button>
+          </div>
+
+          <div className={styles.instructions}>
+            <p>
+              Contact <Link href="tel:+919409656394">+91 9409 6563 94</Link> or{" "}
+              <Link href="mailto:lharsh175@gmail.com">lharsh175@gmail.com</Link>{" "}
+              if you face any difficulties.{" "}
+            </p>
+          </div>
+        </form>
+      </body>
+    </html>
+  );
 }
+
+const mapStateToProps = (state) => ({
+  userInfo: state,
+});
+
+const mapDispatchToProps = {
+  setInfo: setInfo,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
